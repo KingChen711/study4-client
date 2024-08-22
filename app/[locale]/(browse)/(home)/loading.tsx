d0e53ed7976b4d40ccbf123c95f400228091cc2d
Trần Trương Văn <kingchenobama711@gmail.com>
2024-08-23 06:11:56 +0700

    add some loading

---
+++ app/[locale]/(browse)/(home)/loading.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+import { HeroSkeleton } from "./_components/hero"
+import { TestListSkeleton } from "./_components/newest-tests/test-list"
+import { TitleSkeleton } from "./_components/newest-tests/title"
+import { WelcomeSkeleton } from "./_components/welcome"
+
+function HomeLoadingPage() {
+  //TODO: Sync with home page update
+  return (
+    <main>
+      <section className="my-6 flex flex-col gap-y-6">
+        <WelcomeSkeleton />
+        <HeroSkeleton />
+      </section>
+      <div className="mt-8 flex flex-col">
+        <TitleSkeleton />
+        <TestListSkeleton />
+      </div>
+    </main>
+  )
+}
+
+export default HomeLoadingPage
