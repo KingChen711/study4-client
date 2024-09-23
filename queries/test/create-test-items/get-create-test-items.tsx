3368dbece26a021a7fac56bbef558eceb329f712
Trần Trương Văn <kingchenobama711@gmail.com>
2024-09-23 14:45:00 +0700

    save

--- queries/test/create-test-items/get-create-test-items.tsx
+++ queries/test/create-test-items/get-create-test-items.tsx
@@ -1,6 +1,10 @@
 import { cache } from "react"
 
+import prep4Api from "@/lib/prep4-api"
+
 import "server-only"
+
+import { auth } from "@clerk/nextjs/server"
 
 export type TestCategory = {
   testCategoryId: number
@@ -24,138 +28,24 @@ export type CreateTestItems = {
 }
 
 const getCreateTestItem = cache(async (): Promise<CreateTestItems | null> => {
-  // try {
-  //   const { data } = await prep4Api.get<{ data: CreateTestItems }>(
-  //     `/api/tests/create/detail`
-  //   )
+  const { getToken } = auth()
+  try {
+    const { data } = await prep4Api.get<{ data: CreateTestItems }>(
+      `/api/tests/create/detail`,
+      {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      }
+    )
 
-  //   return data.data || null
-  // } catch (error) {
-  //   return null
-  // }
-  return {
-    testCategories: [
-      {
-        testCategoryId: 1,
-        testCategoryName: "IELTS Academic",
-      },
-      {
-        testCategoryId: 2,
-        testCategoryName: "IELTS General",
-      },
-    ],
-    tags: [
-      {
-        tagId: 1,
-        tagName: "IELTS Academic",
-      },
-      {
-        tagId: 2,
-        tagName: "IELTS General",
-      },
-      {
-        tagId: 3,
-        tagName: "Reading",
-      },
-      {
-        tagId: 4,
-        tagName: "Listening",
-      },
-      {
-        tagId: 5,
-        tagName: "Writing",
-      },
-      {
-        tagId: 6,
-        tagName: "Speaking",
-      },
-    ],
-    partitionTags: [
-      {
-        partitionTagId: 1,
-        partitionTagDesc: "[Listening] Note/Form Completion",
-      },
-      {
-        partitionTagId: 2,
-        partitionTagDesc: "[Listening] Table Completion",
-      },
-      {
-        partitionTagId: 3,
-        partitionTagDesc: "[Listening] Multiple Choice",
-      },
-      {
-        partitionTagId: 4,
-        partitionTagDesc: "[Listening] Note/Form Completion",
-      },
-      {
-        partitionTagId: 5,
-        partitionTagDesc: "[Listening] Table Completion",
-      },
-      {
-        partitionTagId: 6,
-        partitionTagDesc: "[Listening] Summary/Flow chart Completion",
-      },
-      {
-        partitionTagId: 7,
-        partitionTagDesc: "[Listening] Matching",
-      },
-      {
-        partitionTagId: 8,
-        partitionTagDesc: "[Listening] Note/Form Completion",
-      },
-      {
-        partitionTagId: 9,
-        partitionTagDesc: "[Listening] Table Completion",
-      },
-      {
-        partitionTagId: 10,
-        partitionTagDesc: "[Listening] Multiple Choice",
-      },
-      {
-        partitionTagId: 11,
-        partitionTagDesc: "[Listening] Note/Form Completion",
-      },
-      {
-        partitionTagId: 12,
-        partitionTagDesc: "[Listening] Table Completion",
-      },
-      {
-        partitionTagId: 13,
-        partitionTagDesc: "[Listening] Summary/Flow chart Completion",
-      },
-      {
-        partitionTagId: 14,
-        partitionTagDesc: "[Listening] Matching",
-      },
-      {
-        partitionTagId: 15,
-        partitionTagDesc: "[Listening] Note/Form Completion",
-      },
-      {
-        partitionTagId: 16,
-        partitionTagDesc: "[Listening] Table Completion",
-      },
-      {
-        partitionTagId: 17,
-        partitionTagDesc: "[Listening] Multiple Choice",
-      },
-      {
-        partitionTagId: 18,
-        partitionTagDesc: "[Listening] Note/Form Completion",
-      },
-      {
-        partitionTagId: 19,
-        partitionTagDesc: "[Listening] Table Completion",
-      },
-      {
-        partitionTagId: 20,
-        partitionTagDesc: "[Listening] Summary/Flow chart Completion",
-      },
-      {
-        partitionTagId: 21,
-        partitionTagDesc: "[Listening] Matching",
-      },
-    ],
+    console.log({ data })
+
+    return data.data || null
+  } catch (error) {
+    console.log(error)
+
+    return null
   }
 })
 
