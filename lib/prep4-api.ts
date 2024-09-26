71a74e0a80f7f2bd685b031a90b1d8e0e57d4cd2
Trần Trương Văn <kingchenobama711@gmail.com>
2024-09-26 21:04:20 +0700

    add server api url

--- lib/prep4-api.ts
+++ lib/prep4-api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const prep4Api = axios.create({
-  baseURL: "http://localhost:7000",
+  baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
